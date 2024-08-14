e816ca4a771ffdb1e3921a7b22728659687f6717
Suneel-Ahmed <suneelahmed413@gmail.com>
2024-08-15 02:08:03 +0500

    feat : add next app

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='w-full h-screen flex justify-center items-center bg-red-400 text-[60px] text-white' >page</div>
+  )
+}
+
+export default page
